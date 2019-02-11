3f983e7b1cde96ef0f4a7105cbfc9845dd5469b8
Nick Phelps <Nick90052@Gmail.com>
2019-02-10 21:46:45 -0600

    complete

--- list-group/app.js
+++ list-group/app.js
@@ -1,10 +1,42 @@
 let ListGroupItem = () => {
-    return <span>Make this component render like a Bootstrap list group item: <a href="https://getbootstrap.com/docs/4.1/components/list-group/" target="_blank">https://goo.gl/images/mY5Qdv</a></span>
+    return <span><ul class="list-group">
+    <li className="list-group-item active">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+    <li className="list-group-item">Morbi leo risus</li>
+    <li className="list-group-item">Porta ac consectetur ac</li>
+    <li className="list-group-item">Vestibulum at eros</li>
+  </ul><br></br>
+  </span>
 }
 
 let ListGroup = () => {
-    return <span>Make this Component render a handful of ListGroupItems</span>
+    return <span><ListGroupItem /><br></br>
+                <ListGroupItem />
+            </span>
 }
 
+const InputForm = () => {
+    return <span>
+        <form>
+            Please Enter number of Bootstrap List Groups: <input id="userInput" type="text"></input>
+            <input id="submitButton" type="button" value="Enter" />
+        </form><br></br>
+    </span>
+}
 
-ReactDOM.render(<ListGroup />, document.getElementById('root'))
+const UserInput = () => {
+        const userInputValue = document.getElementById('userInput').value
+        console.log(userInputValue)
+        let myRenderHTML = []
+        for (let i = 0; i < userInputValue; i++) {
+            myRenderHTML.push(<ListGroupItem />)
+        }      
+        myRenderHTML.unshift(<InputForm />)
+        myRenderHTML.join('')
+        ReactDOM.render(myRenderHTML, document.getElementById('user-input'))
+}
+
+ReactDOM.render(<InputForm />, document.getElementById('user-input'))
+
+const submitButton = document.getElementById('submitButton')
+submitButton.addEventListener('click', UserInput)
